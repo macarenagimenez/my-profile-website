f1f60d54598f79d9376e8ce909a2cb7549c9f266
macarenagimenez <107077871+macarenagimenez@users.noreply.github.com>
2023-01-13 11:18:39 -0300

    About me on header

--- src/Header.js
+++ src/Header.js
@@ -1,20 +1,43 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHand,
+  faSeedling,
+  faCamera,
+} from "@fortawesome/free-solid-svg-icons";
 export default function Header() {
   return (
     <div className="Header">
       {" "}
       <Row>
+        <Col>
+          <h1>About me</h1>
+          <div className="Description">
+            {" "}
+            <p>
+              <FontAwesomeIcon icon={faHand} /> I'm a creative, responsable and
+              commited person. I like to work as a team and solve problems.
+            </p>
+            <p>
+              <FontAwesomeIcon icon={faSeedling} /> Vegetarian. I like
+              travelling, the design and the good food.
+            </p>
+            <p>
+              <FontAwesomeIcon icon={faCamera} /> Photographe amateur.
+            </p>
+          </div>
+        </Col>
         <Col className="Presentation">
+          {" "}
           <img src={require("./hello.png")} className="Hello" alt="hello" />
           <img src={require("./me2.png")} className="Me" alt="me" />
           <img
             src={require("./happyface.png")}
             className="Happyface"
             alt="emoji"
-          />
+          />{" "}
         </Col>
         <Col>
           {" "}
--- src/Main.js
+++ src/Main.js
@@ -1,32 +1,13 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faHand,
-  faSeedling,
-  faCamera,
-} from "@fortawesome/free-solid-svg-icons";
+
 import Timeline from "./timeline.svg";
 import "./Main.css";
 
 export default function Main() {
   return (
     <div className="AboutMe">
-      <h1>About me!</h1>
-      <div className="Description">
-        <p>
-          <FontAwesomeIcon icon={faHand} /> I'm a creative, responsable and
-          commited person. I like to work as a team and solve problems.
-        </p>
-        <p>
-          <FontAwesomeIcon icon={faSeedling} /> Vegetarian. I like travelling,
-          the design and the good food.
-        </p>
-        <p>
-          <FontAwesomeIcon icon={faCamera} /> Photographe amateur.
-        </p>
-      </div>
       <div>
-        <h1>Timeline!</h1>
+        <h1>Timeline</h1>
         <img className="Timeline" src={Timeline} alt="Timeline" />
       </div>
     </div>
