7727b01a116d732456e3b036ead4aae6c2ccee0c
macarenagimenez <107077871+macarenagimenez@users.noreply.github.com>
2023-04-27 17:06:47 -0300

    scroll timeline horizontal

--- src/LineaDeTiempo.js
+++ src/LineaDeTiempo.js
@@ -1,32 +1,80 @@
 import React from "react";
-import Timeline from "./img/timeline2-01.svg";
-import { useRef } from "react";
-
 import "./LineadeTiempo.css";
 import Arrow from "./reuse_components/Arrow";
-import { useInView } from "framer-motion";
+import { useInView, motion, useScroll } from "framer-motion";
+import { useRef } from "react";
 
 export default function LineadeTiempo() {
   const ref = useRef(null);
-  const isInView = useInView(ref, {
-    amount: "some",
-  });
+  const { scrollXProgress } = useScroll({ container: ref });
+  // const isInView = useInView(ref, {
+  //   amount: "some",
+  // });
 
   return (
     <div>
       <section id="timeline" ref={ref}>
-        <div className="Timeline">
+        <div>
           <Arrow tittle="Timeline" />
-          <div className="scrollingHorizontal">
-            <div
-              style={{
-                transform: isInView ? "none" : "translateX(-200px)",
-                opacity: isInView ? 1 : 0,
-                transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
-              }}
-            >
-              <img src={Timeline} alt="Timeline" className="TimelineImg" />{" "}
-            </div>
+
+          <div
+            // style={{
+            //   transform: isInView ? "none" : "translateX(-200px)",
+            //   opacity: isInView ? 1 : 0,
+            //   transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+            // }}
+            className="Timeline"
+          >
+            <ul ref={ref}>
+              <li>
+                {" "}
+                <img
+                  src="/images/timeline-01.svg"
+                  alt="start_university"
+                  className="TimelineImg"
+                />{" "}
+              </li>
+              <li>
+                {" "}
+                <img
+                  src="/images/timeline-02.svg"
+                  alt="Degree_Architecture"
+                  className="TimelineImg"
+                />{" "}
+              </li>
+              <li>
+                {" "}
+                <img
+                  alt="Architecture"
+                  src="/images/timeline-03.svg"
+                  className="TimelineImg"
+                />{" "}
+              </li>
+              <li>
+                {" "}
+                <img
+                  alt="Gaia_Cirila"
+                  src="/images/timeline-04.svg"
+                  className="TimelineImg"
+                />{" "}
+              </li>
+              <li>
+                {" "}
+                <img
+                  src="/images/timeline-05.svg"
+                  alt="change_career"
+                  className="TimelineImg"
+                />{" "}
+              </li>
+              <li>
+                {" "}
+                <img
+                  src="/images/timeline-06.svg"
+                  alt="nowadays"
+                  className="TimelineImg"
+                />{" "}
+              </li>
+            </ul>
           </div>
         </div>
       </section>
